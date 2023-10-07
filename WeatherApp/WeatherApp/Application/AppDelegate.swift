8f608cd566bbcb0773e0a23270edf07958dc017f
Liza Kryshkovskaya <liza.kryshkovskaya@gmail.com>
2023-10-07 14:41:55 +0200

    Create initial project

---
+++ WeatherApp/WeatherApp/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Liza Kryshkovskaya on 7.10.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
